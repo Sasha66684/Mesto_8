44092c3e757dc0132dfb577f691c5549c951e503
Sasha Shapovalov <sovremennik.sp@gmail.com>
2024-06-05 18:17:18 +0300

    added a name verification test

--- src/test/java/Mesto1Test.java
+++ src/test/java/Mesto1Test.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class Mesto1Test {
 
@@ -15,7 +16,15 @@ public class Mesto1Test {
     public void setUp() {
         RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
     }
-
+    @Test
+    @DisplayName("Check user name")
+    @Description("This test is for check current user's name.")
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken) // Передаём токен для аутентификации
+                .get("/api/users/me") // Делаем GET-запрос
+                .then().assertThat().body("data.name", equalTo("Incorrect Name")); // Проверяем, что имя соответствует ожидаемому
+    }
     @Test
     @DisplayName("Add a new photo")
     @Description("This test is for adding a new photo to Mesto.")
@@ -64,5 +73,6 @@ public class Mesto1Test {
                 .delete("/api/cards/{photoId}/likes", photoId) // Делаем DELETE-запрос
                 .then().assertThat().statusCode(200); // Проверяем, что сервер вернул код 200
     }
+
 
 }
